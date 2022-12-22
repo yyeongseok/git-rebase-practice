3af0ea593d8c3fb3f3623afb0b10e6b66240b5d9
yyeongseok <yeongseok0219@gmail.com>
2022-12-22 15:16:18 +0900

    Add:로그인기능 구현

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -31,7 +31,28 @@ app.get('/ping', (req, res) => {
 feature/signin 브랜치의 경우 app.post('/users/signin', ...)
 feature/signup 브랜치의 경우 app.post('/users/signup', ...)
 */
+app.post('/users/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await appDataSource.query(`
+    SELECT
+      users.id
+      users.password
+    FROM
+      users
+    WHERE
+      users.email = ?
+  `, [email]);
+  
+  if (!user) {
+    return res.json({message: "SIGNUP_REQUIRED"})
+  };
 
+  if (!(user[0].password === password)) {
+    return res.json({message: "INVALID_PASSWORD"})
+  };
+  
+  return res.json({ userId: user.id});
+})
 
 app.listen(PORT, () => {
   appDataSource.initialize()
