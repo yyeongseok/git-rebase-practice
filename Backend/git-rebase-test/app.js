a924165974716e80e999369042069e21763b8fe3
yyeongseok <yeongseok0219@gmail.com>
2022-12-22 15:10:00 +0900

    Add:회원가입기능 구현

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -54,6 +54,24 @@ app.post('/users/signin', async (req, res) => {
   return res.json({ userId: user.id});
 })
 
+app.post('/users/signup', async (req, res) => {
+  const { username, email, password } = req.body
+    return await appDataSource.query(`
+      INSERT INTO
+        users (
+          username,
+          email,
+          password			
+        )
+      VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+      )
+    `, [username, email, password])
+})
+
 app.listen(PORT, () => {
   appDataSource.initialize()
     .then(() => {
